7c7d6815148d032255b584ded04ae004c10bfcfc
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2023-06-13 00:02:44 -0500

    Revert "Decompact 1.18.x"
    This reverts commit 3edc6e60ed1531b148cd2eeca7df3d8d63dcc300.

---
+++ src/main/java/com/jetpacker06/craftmobs/CraftMobs.java
@@ -0,0 +1,17 @@
+package com.jetpacker06.craftmobs;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+@Mod(CraftMobs.MOD_ID)
+public class CraftMobs {
+    public static final String MOD_ID = "craftmobs";
+
+    public CraftMobs() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
