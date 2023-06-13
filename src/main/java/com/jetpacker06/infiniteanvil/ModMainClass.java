c9d3c9b7082d13d71b94d933e2310e066165d334
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2023-06-13 12:42:06 -0500

    Infinite Anvil 1.18.x

---
+++ src/main/java/com/jetpacker06/infiniteanvil/ModMainClass.java
@@ -0,0 +1,23 @@
+package com.jetpacker06.infiniteanvil;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AnvilRepairEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+@Mod(ModMainClass.MOD_ID)
+@Mod.EventBusSubscriber
+@SuppressWarnings("unused")
+public class ModMainClass {
+    public static final String MOD_ID = "infiniteanvil";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public ModMainClass() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    @SubscribeEvent
+    public static void onAnvilUse(AnvilRepairEvent event) {
+        event.setBreakChance(0f);
+    }
+}
