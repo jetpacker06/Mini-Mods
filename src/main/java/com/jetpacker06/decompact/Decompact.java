e789c89ea67af409af35ee9b41f18d9cc50fb821
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2023-06-13 00:15:32 -0500

    Decompact 1.19.x

---
+++ src/main/java/com/jetpacker06/decompact/Decompact.java
@@ -0,0 +1,14 @@
+package com.jetpacker06.decompact;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Decompact.MOD_ID)
+public class Decompact {
+    public static final String MOD_ID = "decompact";
+
+    public Decompact() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
+//TODO warden recipe, configurable. by version 2.2
