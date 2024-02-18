0989c4bf6351ad439ede13f690749b284730af5a
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2024-02-18 17:07:17 -0600

    Golden Piggy Charm 1.19.4

--- src/main/java/com/jetpacker06/modpackage/GoldenPiggyCharm.java
+++ src/main/java/com/jetpacker06/modpackage/GoldenPiggyCharm.java
@@ -1,9 +1,12 @@
 package com.jetpacker06.modpackage;
 
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
@@ -16,11 +19,12 @@ import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Mod(GoldenPiggyCharm.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GoldenPiggyCharm {
     public static final String MOD_ID = "goldenpiggycharm";
 
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GoldenPiggyCharm.MOD_ID);
-    public static RegistryObject<Item> ITEM = ITEMS.register("golden_piggy_charm", () -> new GoldenPiggyCharmItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static RegistryObject<Item> ITEM = ITEMS.register("golden_piggy_charm", () -> new GoldenPiggyCharmItem(new Item.Properties()));
 
     public GoldenPiggyCharm() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -33,5 +37,12 @@ public class GoldenPiggyCharm {
     private static void onInterModEnqueue(final InterModEnqueueEvent event) {
         InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE,
                 () -> SlotTypePreset.CHARM.getMessageBuilder().build());
+    }
+
+    @SubscribeEvent
+    public static void populateTab(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(new ItemStack(ITEM.get()));
+        }
     }
 }
