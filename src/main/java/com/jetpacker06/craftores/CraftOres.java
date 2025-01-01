7ff1b812155e03bc4d8ecacd8e3cbf2069d3b6cc
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2025-01-01 14:19:43 -0600

    CraftOres 1.18.2

---
+++ src/main/java/com/jetpacker06/craftores/CraftOres.java
@@ -0,0 +1,17 @@
+package com.jetpacker06.craftores;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(CraftOres.MOD_ID)
+public class CraftOres {
+    public static final String MOD_ID = "craftores";
+    public CraftOres() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
